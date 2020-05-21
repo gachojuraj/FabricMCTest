9388d79bc49c8f6c4eab9b90333c460e86b3b9ed
gachojuraj <mmamma151@gmail.com>
2020-05-21 23:37:13 +0200

    added simple HP indicator

--- src/main/java/sk/jurij/fabrictest/mixin/RenderMixin.java
+++ src/main/java/sk/jurij/fabrictest/mixin/RenderMixin.java
@@ -1,18 +1,23 @@
 package sk.jurij.fabrictest.mixin;
 
-import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Camera.class)
+@Mixin(EntityRenderer.class)
 public abstract class RenderMixin{
-
-    @Inject(at = @At("HEAD"), method = "update(Lnet/minecraft/world/BlockView;Lnet/minecraft/entity/Entity;ZZF)V")
-    public void update(BlockView area, Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo info) {
-        //Not used yet
+    private String getHealth(Entity entity){
+        return String.valueOf((int)Math.ceil(Double.parseDouble(entity.getDataTracker().get(entity.getDataTracker().getAllEntries().get(8).getData()).toString())));
+    }
+    @Shadow protected void renderLabelIfPresent(Entity entity, String string, MatrixStack matrices, VertexConsumerProvider vertexConsumerProvider, int i){}
+    @Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/entity/Entity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
+    public void render(Entity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
+        if (entity.isLiving()) this.renderLabelIfPresent(entity, getHealth(entity), matrices, vertexConsumers, light);
     }
 }
